dd420660c75a4d6af08472d0d85788003f46bf77
samuelreichor <samuelreichor@gmail.com>
2022-12-06 15:22:16 +0100

    Init Vue Template

---
+++ src/App.vue
@@ -0,0 +1,35 @@
+<script setup>
+// This starter template is using Vue 3 <script setup> SFCs
+// Check out https://vuejs.org/api/sfc-script-setup.html#script-setup
+import HelloWorld from './components/HelloWorld.vue'
+</script>
+
+<template>
+  <div class="container mx-auto bg-gray-200 rounded-xl shadow border p-8 m-10">
+    <p class="text-3xl text-gray-700 font-bold mb-5">
+      Welcome!
+    </p>
+    <p class="text-black text-lg">
+      Vue and Tailwind CSS in action
+    </p>
+    <div class="aspect-w-16 aspect-h-3">
+      <iframe src="https://www.youtube.com/embed/dQw4w9WgXcQ" frameborder="0"
+        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+        allowfullscreen></iframe>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.logo {
+  height: 6em;
+  padding: 1.5em;
+  will-change: filter;
+}
+.logo:hover {
+  filter: drop-shadow(0 0 2em #646cffaa);
+}
+.logo.vue:hover {
+  filter: drop-shadow(0 0 2em #42b883aa);
+}
+</style>
---
+++ theme/colors.js
@@ -0,0 +1,25 @@
+/*
+ |--------------------------------------------------------------------------
+ |  Color Settings (Tailwind)
+ |--------------------------------------------------------------------------
+ */
+
+const colors = require('tailwindcss/colors');
+
+module.exports = {
+    transparent: 'transparent',
+    black: colors.black,
+    white: colors.white,
+    primary: {
+        DEFAULT: '#ff49db',
+    },
+    secondary: {
+        DEFAULT: '#ff49db',
+    },
+    error: {
+        DEFAULT: '#ff49db',
+    },
+    success: {
+        DEFAULT: '#ff49db',
+    },
+};
